410dd406620505b6a94c9e6360af60924a7626d0
gbvars988 <ronaldyho91@gmail.com>
2024-12-30 23:17:00 +1100

    adds required keyword to enforce non-nullable property Title

--- backend/todoBackend/Models/ToDo.cs
+++ backend/todoBackend/Models/ToDo.cs
@@ -2,6 +2,6 @@
 public class ToDo
 {
     public int Id { get; set; }
-    public string Title { get; set; }
+    public required string Title { get; set; }
     public string ?Body { get; set; }
 }
