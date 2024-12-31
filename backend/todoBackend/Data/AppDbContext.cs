72f01da45cb37017210fa029cd99cd56c0e99799
gbvars988 <ronaldyho91@gmail.com>
2024-12-31 19:00:36 +1100

    adds AuthController.cs and user model

--- backend/todoBackend/Data/AppDbContext.cs
+++ backend/todoBackend/Data/AppDbContext.cs
@@ -1,7 +1,9 @@
-using Microsoft.EntityFrameworkCore; 
+using Microsoft.EntityFrameworkCore;
 
 public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
     public DbSet<ToDo> ToDos { get; set; }
+
+    public DbSet<User> Users { get; set; }
 }
