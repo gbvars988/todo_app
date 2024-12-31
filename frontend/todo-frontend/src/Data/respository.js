67765a3594eebcdc05601c188abe5700208429ce
gbvars988 <ronaldyho91@gmail.com>
2024-12-31 15:19:10 +1100

    adds getAllToDos and deleteToDo axios functions

--- frontend/todo-frontend/src/Data/respository.js
+++ frontend/todo-frontend/src/Data/respository.js
@@ -3,12 +3,22 @@ import axios from "axios";
 const API_HOST = "http://localhost:5140";
 
 async function addToDo(todo) {
-  const response = await axios.post(API_HOST + "/todo/add", todo, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  const response = await axios.post(API_HOST + "/todo/add", todo);
+  // headers: {
+  //   "Content-Type": "application/json", // not required tbh axios does it auto
+  // },
+
   return response.data;
 }
 
-export { addToDo };
+async function getAllToDos() {
+  const response = await axios.get(API_HOST + "/todo/get");
+  return response.data;
+}
+
+async function deleteToDo(id) {
+  const response = await axios.delete(API_HOST + `/todo/delete/${id}`);
+  return response.data;
+}
+
+export { addToDo, getAllToDos, deleteToDo };
