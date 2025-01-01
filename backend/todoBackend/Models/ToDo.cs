91b1f15876b157f49f9edf2d39e9d9733940294c
gbvars988 <ronaldyho91@gmail.com>
2025-01-01 14:02:18 +1100

    adds UserId as FK to ToDo table

--- backend/todoBackend/Models/ToDo.cs
+++ backend/todoBackend/Models/ToDo.cs
@@ -3,5 +3,7 @@ public class ToDo
 {
     public int Id { get; set; }
     public required string Title { get; set; }
-    public string ?Body { get; set; }
+    public string? Body { get; set; }
+
+    public int UserId { get; set; }
 }
