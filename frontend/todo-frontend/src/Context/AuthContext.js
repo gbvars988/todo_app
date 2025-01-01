eac68fa219ce49837877ba917f420ba53cd7b101
gbvars988 <ronaldyho91@gmail.com>
2025-01-01 16:01:42 +1100

    adds files for AuthContext and ProtectedRooute

---
+++ frontend/todo-frontend/src/Context/AuthContext.js
@@ -0,0 +1,25 @@
+import React, { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null); // User state null = logged out
+
+  const login = (token) => {
+    localStorage.setItem("authToken", token);
+    setUser({ token });
+  };
+
+  const logout = () => {
+    localStorage.removeItem("authToken");
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
