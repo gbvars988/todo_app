13f86f3ef56a8c6827b1c5c675e6b6c65f40c570
gbvars988 <ronaldyho91@gmail.com>
2025-01-01 15:59:55 +1100

    adds routing to App.js

--- frontend/todo-frontend/src/App.js
+++ frontend/todo-frontend/src/App.js
@@ -1,11 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./Components/Home";
 import ToDoList from "./Components/ToDoList";
+import ProtectedRoute from "./Context/ProtectedRoute";
 
 function App() {
   return (
-    <div className="App">
-      <ToDoList></ToDoList>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/todo"
+          element={
+            <ProtectedRoute>
+              <ToDoList />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
