c973550c6657250c9b3f091d0e0843600fee3818
gbvars988 <ronaldyho91@gmail.com>
2025-01-01 15:56:17 +1100

    new migration files for db

---
+++ backend/todoBackend/Migrations/20250101040329_AddUserIdFKtoToDoTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace todoBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserIdFKtoToDoTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "ToDos",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "ToDos");
+        }
+    }
+}
