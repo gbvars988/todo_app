700c2fc60448592bf95b0160665c28a7c0740ba7
gbvars988 <ronaldyho91@gmail.com>
2025-01-01 16:29:28 +1100

    fixes registration behaviour when registration unsuccessful

--- frontend/todo-frontend/src/Components/Register.js
+++ frontend/todo-frontend/src/Components/Register.js
@@ -1,16 +1,22 @@
 import React, { useState } from "react";
-import { Register } from "../Data/respository";
+import { RegisterUser } from "../Data/respository";
+import { useNavigate } from "react-router-dom";
 
 export default function Register() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   const handleRegister = async () => {
     try {
-      const data = Register({ username, password });
-      if (data) console.log("success");
+      const response = await RegisterUser({ username, password });
+      if (response && response.status === 200) {
+        alert("Registration successful!");
+        navigate("/");
+      }
     } catch (error) {
       console.error("Something went wrong", error);
+      alert("Registration failed. User may already exist");
     }
   };
 
