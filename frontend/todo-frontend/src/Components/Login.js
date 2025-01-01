3fdf2a394193c1bea95f5a963f870e18ccbed07f
gbvars988 <ronaldyho91@gmail.com>
2025-01-01 15:58:31 +1100

    Login uses auth context

--- frontend/todo-frontend/src/Components/Login.js
+++ frontend/todo-frontend/src/Components/Login.js
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
 import { LoginUser } from "../Data/respository";
+import { useAuth } from "../Context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const { login } = useAuth();
+  const navigate = useNavigate();
 
   const handleLogin = async () => {
     try {
       const token = await LoginUser({ username, password });
       if (token) {
-        localStorage.setItem("authToken", token);
+        login(token);
         alert("Login successful!");
+        navigate("/todo");
       }
     } catch (error) {
       console.error("failed to login", error);
