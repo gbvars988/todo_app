7a3dd0237cf50067bd863dc604338a769ed7a6b0
gbvars988 <ronaldyho91@gmail.com>
2025-01-02 14:26:00 +1100

    Adds override method to create default value for field Status in ToDo Table

--- backend/todoBackend/Data/AppDbContext.cs
+++ backend/todoBackend/Data/AppDbContext.cs
@@ -6,4 +6,14 @@ public class AppDbContext : DbContext
     public DbSet<ToDo> ToDos { get; set; }
 
     public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder); // Always call the base method
+
+        modelBuilder.Entity<ToDo>()
+            .Property(t => t.Status)
+            .HasDefaultValue("todo");
+    }
+
 }
