aa4cf5d9aaec838fbe56126cc42a84e2e258ad1b
gbvars988 <ronaldyho91@gmail.com>
2025-01-02 14:23:31 +1100

    new db migration to add Status field to ToDos table

---
+++ backend/todoBackend/Migrations/20250102024140_AddDefaultValueToStatus.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace todoBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDefaultValueToStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "ToDos",
+                type: "text",
+                nullable: false,
+                defaultValue: "todo");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "ToDos");
+        }
+    }
+}
