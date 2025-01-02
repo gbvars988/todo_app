94ff9a30c617ba1691316d0a1687007bb6a6d901
gbvars988 <ronaldyho91@gmail.com>
2025-01-02 14:24:42 +1100

    adds new DTO for updating ToDo status

--- backend/todoBackend/Models/ToDo.cs
+++ backend/todoBackend/Models/ToDo.cs
@@ -1,9 +1,15 @@
 
 public class ToDo
 {
+    public ToDo()
+    {
+        Status = "todo";
+    }
+
     public int Id { get; set; }
     public required string Title { get; set; }
     public string? Body { get; set; }
-
     public int UserId { get; set; }
+
+    public string Status { get; set; }
 }
